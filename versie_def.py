ccd115f5ced8ccc83f2759c9b0a86451fa76b61d
MehmetNL <125833460+MehmetNL@users.noreply.github.com>
2024-05-09 21:26:46 +0200

    Update versie_def.py

--- versie_def.py
+++ versie_def.py
@@ -9,6 +9,6 @@ def versie_info():
 # versie 1.7 resultaat aantallen in getallen, en vervolgens lijst geven met de ingevoerde resultaat.
 # versie 1.8.1 Bij auto toevoegen worden gegevens nu vannuit rdw opgehaald, alleen kenteken invullen is voldoende,
 # indien ontbrekende gegevens worden opgehaald dient gebruiker zelf in te vullen.
-# versie 1.8.2 Bij wat oudere auto, zijn geen cataloog prijs bekend bij rdw, database verwacht daar wel een waarde ipv None
+# versie 1.8.2 Oudere voertuigen, zijn geen cataloog prijs bekend bij rdw, database verwacht daar wel een waarde ipv None
 # if toegepast, indien RDW API None waarde terug keert, wordt de cat. waarde 0 gezet.
 # versie 1.8.3 NN in databse opgevangen met user input
